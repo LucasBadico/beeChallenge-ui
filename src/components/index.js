11d1fd97be92a98e2a32e8b40725e994fd982868
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-28 16:02:33 -0300

    module added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Button'
 export * from './Form'
 export * from './Titles'
+export * from './Module'
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import styles from './styles'
-import { css } from 'aphrodite'
 
 import {
   Button,
   TextInput,
   TitleModule,
+  TitlePage,
+  Module,
 } from 'components'
 
 
 export default (
   <Switch>
-    <Route path='/hello' component={() => <div className={css(styles.body)}>  <TitleModule>HELLO FROM REACT!</TitleModule> </div>} />
+    <Route path='/hello' component={() =>(
+      <div>
+        <TitlePage>HELLO FROM REACT!</TitlePage>
+        <Module>
+          <form>
+          <TitleModule> teste de titulo</TitleModule>
+          <TextInput type="text" placeholder="Um exemplo de input"/>
+          <TextInput type="text" placeholder="Um exemplo de input"/>
+          <Button> Exemplo de botão </Button>
+          </form>
+        </Module>
+      </div>)
+    } />
     <Route path='/button' component={() => <Button> Exemplo de botão </Button>} />
     <Route path='/text-input' component={() => <TextInput type="text" placeholder="Um exemplo de input"/>} />
     <Route path='/styled-title' component={() => <TitleModule> teste de titulo</TitleModule>} />
