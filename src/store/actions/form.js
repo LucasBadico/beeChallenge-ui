40c8b96e5aded0c16d8e223a5671cdbb4f61a103
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-29 10:13:30 -0300

    will aplied to form store

--- src/store/actions/form.js
+++ src/store/actions/form.js
@@ -1,10 +1,11 @@
 import log from 'log'
 export const SAVE_FORM_FIELD = 'SAVE_FORM_FIELD'
 export const NOT_FINDED_DDD = 'NOT_FINDED_DDD'
+export const WILL_SAVE_FORM_FIELD = 'WILL_SAVE_FORM_FIELD'
 
 export function saveFormField(form, field, value) {
     return {
-        type: SAVE_FORM_FIELD,
+        type: WILL_SAVE_FORM_FIELD,
         form,
         field,
         value,
--- src/store/epics/form.js
+++ src/store/epics/form.js
@@ -2,16 +2,58 @@ import 'rxjs'
 
 import { race } from 'rxjs/observable/race'
 import { timer } from 'rxjs/observable/timer'
-import { SAVE_FORM_FIELD, OPEN_FORM, NOT_FINDED_DDD } from 'store/actions'
+import {
+    WILL_SAVE_FORM_FIELD,
+    SAVE_FORM_FIELD,
+    OPEN_FORM,
+    NOT_FINDED_DDD,
+} from 'store/actions'
+
 import log from 'log'
 
 // Caso tenha um valor tal ativa o outro formulario
-export const forms = (action$, store) => action$.ofType(SAVE_FORM_FIELD)
-   .map((props) => {
-       if (props.value === NOT_FINDED_DDD) {
-        return ({ type: OPEN_FORM, form: 'lead' })
-       }
-       return ({ ...props, type: 'SAVE'})
+export const openLeadOnNotFinded = (action$, store) => action$.ofType(WILL_SAVE_FORM_FIELD)
+    .map(action => {
+        if (action.value === NOT_FINDED_DDD && action.form === 'calculator' ) {
+            return ({ type: OPEN_FORM, form: 'lead' })
+        }
+        return {
+            ...action,
+            type: SAVE_FORM_FIELD
+        }
     })
-   .do((...args) => log(args))
+
+export const clearNotFinded = (action$, store) => action$
+    .mergeMap(() => {
+        action$.ofType(OPEN_FORM).map(action => {
+            if (action.form === 'lead') {
+                return {
+                    type: SAVE_FORM_FIELD,
+                    form: 'calculator',
+                    value: null,
+                }
+            }
+            return action
+        }) 
+    })
+// .switchMap(action => action$.ofType(SAVE_FORM_FIELD)
+//     .timer(1000).map(action => {
+//         if (action.value === NOT_FINDED_DDD) {
+//             return {
+//                 type: SAVE_FORM_FIELD,
+//                 ...action,
+//                 value: undefined,
+//             }
+//         }
+//         return action
+//    })
+// )
 //    .ignoreElements()
+/*
+.mergeMap(action =>
+        race(
+        timer(2000).mapTo({ type: OPEN_N_M }),
+        action$.ofType(EPT_CANCEL_OPEN_N_M)
+            .take(1),
+       ),
+*/
